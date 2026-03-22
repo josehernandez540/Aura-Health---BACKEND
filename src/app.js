c335cadf5759861b4ebf463ee9ff39f648553a56
josehernandez540 <jose.hernandez540@pascualbravo.edu.co>
2026-03-22 00:50:21 -0500

    chore(config): add swagger setup, smtp env vars and update app middlewares

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import routes from './presentation/routes/index.js';
-
+import swaggerUi from 'swagger-ui-express';
 import { errorMiddleware } from './presentation/middlewares/error.middleware.js';
 import { env } from './config/env.js';
+import { swaggerSpec } from './config/swagger.js';
 
 export const createApp = () => {
     const app = express();
@@ -18,6 +19,12 @@ export const createApp = () => {
 
     if (env.isDev) {
         app.use(morgan('dev'));
+        app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec,{
+            customSiteTitle: 'Aura Health API Docs',
+            swaggerOptions: {
+                persistAuthorization: true,
+            },
+        }));
     }
 
     app.use("/api", routes);
